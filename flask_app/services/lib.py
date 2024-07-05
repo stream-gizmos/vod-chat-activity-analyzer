b70684d40ef64c88017844d8cccf169bda173119
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2024-07-05 05:08:19 +0500

    Build combined graph for all VODs (aligned by absolute time)

--- flask_app/services/lib.py
+++ flask_app/services/lib.py
@@ -1,8 +1,11 @@
 from hashlib import md5
+from typing import TypeVar
 from urllib.parse import urlparse
 
 import pandas as pd
 import plotly.graph_objects as go
+
+IntervalWindow = TypeVar('IntervalWindow', str, int)
 
 
 def url_to_hash(url: str) -> str:
@@ -21,6 +24,10 @@ def hash_to_times_file(video_hash: str) -> str:
     return f"data/{video_hash}_times.json"
 
 
+def hash_to_timestamps_file(video_hash: str) -> str:
+    return f"data/{video_hash}_timestamps.json"
+
+
 # https://stackoverflow.com/questions/7160737/how-to-validate-a-url-in-python-malformed-or-not
 def is_http_url(url):
     try:
@@ -30,33 +37,45 @@ def is_http_url(url):
         return False
 
 
-def times_data_to_rolling_sums(data, intervals):
+def build_dataframe_by_timestamp(data):
     df = pd.DataFrame(data, columns=["timestamp"])
 
-    # Convert timestamp to timedelta
-    df["timestamp"] = pd.to_timedelta(df["timestamp"], unit="s")
+    # Convert timestamp to time
+    df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True, unit="us")
     # Assign a message count of 1 for each timestamp
     df["messages"] = 1
 
-    # Resample the data into 5 second bins, filling in any missing seconds with 0
     df.set_index("timestamp", inplace=True)
+    df.sort_index(inplace=True)
+
+    return df
+
+
+def make_buckets(df: pd.DataFrame, intervals: list[IntervalWindow]) -> dict[IntervalWindow, pd.DataFrame]:
+    # Resample the data into 5 second bins, filling in any missing seconds with 0
     df = df.resample("5S").sum()
 
+    # Convert absolute time to duration from the video start
+    df["time_in_seconds"] = df.index.to_series().sub(df.index[0])
+    df.set_index("time_in_seconds", inplace=True)
+
+    result = {}
     for interval in intervals:
         df_resampled = df.rolling(interval).sum()
+        result[interval] = df_resampled
 
-        yield interval, df_resampled
+    return result
 
 
-def build_scatter_fig(interval_dataframes: dict, figure_title):
+def build_scatter_fig(scatter_dataframes: dict, figure_title: str, xaxis_title: str):
     fig = go.Figure()
 
-    for interval, interval_df in interval_dataframes.items():
+    for line_name, df in scatter_dataframes.items():
         fig.add_trace(go.Scatter(
-            x=interval_df.index.total_seconds() / 60,  # convert seconds to minutes
-            y=interval_df["messages"],
+            x=df.index.total_seconds() / 60,  # convert seconds to minutes
+            y=df["messages"],
             mode="lines",
-            name=interval,
+            name=line_name,
         ))
 
     fig.update_layout(
@@ -65,7 +84,7 @@ def build_scatter_fig(interval_dataframes: dict, figure_title):
         height=300,
         margin=dict(t=30, b=0, l=0, r=0, pad=5),
         hovermode="x",
-        xaxis_title="Time (in minutes)",
+        xaxis_title=xaxis_title,
         xaxis=dict(
             tickmode="linear",
             tick0=0,
--- flask_app/views/front.py
+++ flask_app/views/front.py
@@ -1,11 +1,12 @@
 import json
 
+import pandas as pd
 import plotly
 from chat_downloader import ChatDownloader
 from flask import Blueprint, render_template, request, redirect, url_for
 
-from flask_app.services.lib import build_scatter_fig, hash_to_chat_file, hash_to_meta_file, hash_to_times_file, \
-    is_http_url, times_data_to_rolling_sums, url_to_hash
+from flask_app.services.lib import build_dataframe_by_timestamp, build_scatter_fig, hash_to_chat_file, \
+    hash_to_meta_file, hash_to_times_file, hash_to_timestamps_file, is_http_url, make_buckets, url_to_hash
 
 front_bp = Blueprint('front', __name__)
 
@@ -43,14 +44,18 @@ def start_download():
         chat = ChatDownloader().get_chat(url, output=hash_to_chat_file(video_hash))
 
         list_of_times = []
+        list_of_timestamp = []
         for message in chat:
             if message["time_in_seconds"] < 0:
                 continue
 
             list_of_times.append(message["time_in_seconds"])
+            list_of_timestamp.append(message["timestamp"])
 
         with open(hash_to_times_file(video_hash), "w") as fp:
             json.dump(list_of_times, fp)
+        with open(hash_to_timestamps_file(video_hash), "w") as fp:
+            json.dump(list_of_timestamp, fp)
 
     hashes_string = ",".join(hashes)
 
@@ -62,23 +67,29 @@ def display_graph(video_hashes):
     video_hashes = video_hashes.split(",")
 
     intervals = ["15S", "60S", "300S"]
+    combined_df: pd.DataFrame | None = None
 
     graphs = {}
     for i, video_hash in enumerate(video_hashes, start=1):
         with open(hash_to_meta_file(video_hash), "r") as fp:
             meta = json.load(fp)
-
-        with open(hash_to_times_file(video_hash), "r") as fp:
+        with open(hash_to_timestamps_file(video_hash), "r") as fp:
             data = json.load(fp)
 
-        interval_dataframes = {}
-        for interval, interval_df in times_data_to_rolling_sums(data, intervals):
-            interval_dataframes[interval] = interval_df
+        df = build_dataframe_by_timestamp(data)
+        combined_df = df.copy() if combined_df is None else combined_df.add(df, fill_value=0)
 
-        fig = build_scatter_fig(interval_dataframes, "Number of messages")
+        interval_dataframes = make_buckets(df, intervals)
 
+        fig = build_scatter_fig(interval_dataframes, "Number of messages", "Video time (in minutes)")
         graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        graphs[f"graph{i:02d}"] = dict(url=meta["url"], json=graph_json)
 
-        graphs[f"graph{i}"] = dict(url=meta["url"], json=graph_json)
+    if len(video_hashes) > 1 and combined_df is not None:
+        interval_dataframes = make_buckets(combined_df, intervals)
+
+        fig = build_scatter_fig(interval_dataframes, "Number of messages", "Stream time (in minutes)")
+        graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        graphs[f"graph{0:02d}"] = dict(caption='Combined stream stats', json=graph_json)
 
     return render_template("graph.html", graphs=graphs)
