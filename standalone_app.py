0b62a66288522d77b25de103bb06eb8eaacb6542
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2024-07-16 07:07:39 +0500

    Use a built-in web-server of pywebview (with SSL)

--- standalone_app.py
+++ standalone_app.py
@@ -1,19 +1,10 @@
-import sys
-import threading
-
 import webview
 
 from flask_app import init_app
 
 webview_name = "Chat Analyzer"
-host = "127.0.0.1"
-flask_port = 8080
 
 flask_app = init_app()
-
-
-def start_web_server():
-    flask_app.run(host=host, port=flask_port)
 
 
 def on_start(window):
@@ -21,20 +12,16 @@ def on_start(window):
 
 
 if __name__ == '__main__':
-    web_server_thread = threading.Thread(target=start_web_server)
-    web_server_thread.daemon = True
-    web_server_thread.start()
-
-    start_url = f"http://{host}:{flask_port}/"
-
     window = webview.create_window(
         webview_name,
-        start_url,
+        flask_app,
         width=800,
         height=600,
         resizable=True,
     )
 
-    webview.start(on_start, window)
-
-    sys.exit()
+    webview.start(
+        on_start,
+        window,
+        ssl=True,
+    )
