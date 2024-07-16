cfd34ed14f460c1ae2c46a9a84e10d0dd94a5aeb
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2024-07-16 05:57:50 +0500

    Start the window maximized

--- standalone_app.py
+++ standalone_app.py
@@ -7,22 +7,34 @@ from flask_app import init_app
 
 webview_name = "Chat Analyzer"
 host = "127.0.0.1"
-port = 8080
+flask_port = 8080
 
 flask_app = init_app()
 
 
 def start_web_server():
-    flask_app.run(host=host, port=port)
+    flask_app.run(host=host, port=flask_port)
+
+
+def on_start(window):
+    window.maximize()
 
 
 if __name__ == '__main__':
     web_server_thread = threading.Thread(target=start_web_server)
     web_server_thread.daemon = True
     web_server_thread.start()
-    print(f"{web_server_thread=}")
 
-    webview.create_window(webview_name, f"http://{host}:{port}/", width=800, height=600, resizable=True)
-    webview.start()
+    start_url = f"http://{host}:{flask_port}/"
+
+    window = webview.create_window(
+        webview_name,
+        start_url,
+        width=800,
+        height=600,
+        resizable=True,
+    )
+
+    webview.start(on_start, window)
 
     sys.exit()
