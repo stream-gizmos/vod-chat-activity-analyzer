b9dff8d211528251d7cd0d862f12d6a1204ef680
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2024-07-19 05:11:49 +0500

    Allow to navigate Twitch video by the chart clicking

--- flask_app/static/scripts.js
+++ flask_app/static/scripts.js
@@ -13,3 +13,40 @@ function toggleVisibility(playerContainerId) {
         $container.classList.add("hidden")
     }
 }
+
+function onPointClick($plot, handler) {
+    $plot.on("plotly_click", function (data) {
+        if (!data.event.shiftKey) {
+            return
+        }
+
+        const lines = data.points.filter(trace => trace.data.mode === "lines")
+        const mainLine = lines.find(trace => trace.data.name === "15s") || lines[0]
+        handler(mainLine.x, mainLine.y)
+    })
+}
+
+/**
+ * @param {number} td
+ * @return {string}
+ */
+function timeDeltaToTime(td) {
+    const sign = td < 0 ? "-" : ""
+    let hours = Math.floor(Math.abs(td) / 3600)
+    const remainder = Math.abs(td) - hours * 3600
+
+    let minutes = Math.floor(remainder / 60)
+    let seconds = remainder - minutes * 60
+
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    if (seconds < 10) {
+        seconds = `0${seconds}`
+    }
+
+    return `${sign}${hours}:${minutes}:${seconds}`
+}
