e22516861c6eaf3153ee29d64f84120e4d62e2f4
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2024-12-30 03:09:55 +0500

    Embed a computed

--- flask_app/static/vue-helpers.js
+++ flask_app/static/vue-helpers.js
@@ -1,17 +1,15 @@
 import {computed} from "vue"
 import Vue3Toasity, {toast} from "vue3-toastify"
 
-const toastifyTheme = computed(() => {
-    if (siteTheme.getCurrentTheme() === "dark") {
-        return toast.THEME.DARK
-    }
-
-    return toast.THEME.LIGHT
-})
-
 export function installCommonPlugins(app) {
     app.use(Vue3Toasity, {
-        theme: toastifyTheme,
+        theme: computed(() => {
+            if (siteTheme.getCurrentTheme() === "dark") {
+                return toast.THEME.DARK
+            }
+
+            return toast.THEME.LIGHT
+        }),
     })
 
     return app
